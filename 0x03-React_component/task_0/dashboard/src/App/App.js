f7461c70e2d16d5db170ee802cc8fb1195a916ba
Rosemwende <rosemuema82@gmail.com>
2024-11-07 10:19:29 +0000

    App.js

--- 0x03-React_component/task_0/dashboard/src/App/App.js
+++ 0x03-React_component/task_0/dashboard/src/App/App.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      message: 'Hello, React!',
-    };
+  static defaultProps = {
+    logOut: () => {},
+  };
+
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyDown);
   }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  handleKeyDown = (event) => {
+    if (event.ctrlKey && event.key === 'h') {
+      alert('Logging you out');
+      this.props.logOut();
+    }
+  };
 
   render() {
     return (
       <div>
-        <h1>{this.state.message}</h1>
+        <h1>Welcome to the App!</h1>
       </div>
     );
   }
