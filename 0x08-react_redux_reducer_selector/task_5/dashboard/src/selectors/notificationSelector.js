5e29c42b99bce9a6800baa20730d41526ee19fa4
Rosemwende <rosemuema82@gmail.com>
2024-12-04 15:15:44 +0000

    notificationSelector.js

---
+++ 0x08-react_redux_reducer_selector/task_5/dashboard/src/selectors/notificationSelector.js
@@ -0,0 +1,7 @@
+export const filterTypeSelected = (state) => state.get('filter');
+
+export const getNotifications = (state) => state.get('notifications');
+
+export const getUnreadNotifications = (state) => {
+  return state.get('notifications').filter((notification) => !notification.get('isRead'));
+};
