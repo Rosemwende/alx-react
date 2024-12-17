348518e17ac48d9ec3a049969b21a5f3fb16809a
rosemwende <rosemuema82@gmail.com>
2024-12-17 09:19:10 +0000

    0-fromjs.js

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -3,10 +3,10 @@ import { fromJS } from 'immutable';
 /**
  * Converts an object into an Immutable Map using fromJS
  * @param {Object} object - The object to convert
- * @returns {Map} - Immutable Map representation of the object
+ * @returns {Immutable.Map} - Immutable Map representation of the object
  */
-function getImmutableObject(object) {
-    return fromJS(object);
-}
+const getImmutableObject = (object) => {
+  return fromJS(object);
+};
 
-export { getImmutableObject };
+export default getImmutableObject;
