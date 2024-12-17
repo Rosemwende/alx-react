247b47b138a3c7c7f28536e069b3820c34ce4a16
rosemwende <rosemuema82@gmail.com>
2024-12-17 10:34:17 +0000

    4-mutations.js

--- 0x06-React_Immutable/4-mutations.js
+++ 0x06-React_Immutable/4-mutations.js
@@ -7,8 +7,9 @@ export const map = Map({
   4: 'Oliver',
   5: 'Jacob',
   6: 'Lucas',
-})
+});
 
-export const map2 = map
-  .set(2, 'Benjamin');
-  .set(4, 'Oliver');
+export const map2 = map.withMutations(mapMutate => {
+  mapMutate.set(2, 'Benjamin');
+  mapMutate.set(4, 'Oliver');
+});
