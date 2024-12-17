157cc61e0730fae678bb6eab72a537aff25969d1
rosemwende <rosemuema82@gmail.com>
2024-12-17 09:29:49 +0000

    1-map.js

---
+++ 0x06-React_Immutable/1-map.js
@@ -0,0 +1,12 @@
+import { Map } from 'immutable';
+
+/**
+ * Converts an object into an Immutable Map using Map
+ * @param {Object} object - The object to convert
+ * @returns {Immutable.Map} - Immutable Map representation of the object
+ */
+const getImmutableObject = (object) => {
+  return Map(object);
+};
+
+export default getImmutableObject;
