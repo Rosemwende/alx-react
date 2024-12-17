a99988cad9dc7c0d6b04dd2b183c8fccfcfe8478
rosemwende <rosemuema82@gmail.com>
2024-12-17 11:27:58 +0000

    8-seq.js

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -1,9 +1,8 @@
 import { Seq } from 'immutable';
-
-export function printBestStudents(grades) {
+export default function printBestStudents(grades) {
   Seq(grades)
-    .filter(student => student.score >= 70)
-    .forEach(student => {
+    .filter((student) => student.score >= 70)
+    .forEach((student) => {
       const firstName = student.firstName.charAt(0).toUpperCase() + student.firstName.slice(1);
       const lastName = student.lastName.charAt(0).toUpperCase() + student.lastName.slice(1);
       console.log(`${firstName} ${lastName}`);
