57a339c85c089016787050ac4bef8f2111edb3b0
rosemwende <rosemuema82@gmail.com>
2024-12-23 21:22:08 +0000

    App.test.js

--- 0x03-React_component/task_1/dashboard/src/App/App.test.js
+++ 0x03-React_component/task_1/dashboard/src/App/App.test.js
@@ -1,25 +1,13 @@
 import React from 'react';
-import { render, unmountComponentAtNode } from 'react-dom';
 import { shallow } from 'enzyme';
 import App from './App';
 
 describe('<App />', () => {
-  let container = null;
-
-  beforeEach(() => {
-    container = document.createElement('div');
-    document.body.appendChild(container);
-  });
-
-  afterEach(() => {
-    unmountComponentAtNode(container);
-  });
-
   it('should call logOut when Ctrl + H is pressed', () => {
     const logOutMock = jest.fn();
     const alertMock = jest.spyOn(window, 'alert').mockImplementation(() => {});
 
-    const wrapper = shallow(<App logOut={logOutMock} />, { attachTo: container });
+    const wrapper = shallow(<App logOut={logOutMock} />);
 
     const event = new KeyboardEvent('keydown', { key: 'h', ctrlKey: true });
     window.dispatchEvent(event);
