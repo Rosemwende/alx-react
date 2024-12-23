e918398250afff568e45ad652190215bcd1de772
rosemwende <rosemuema82@gmail.com>
2024-12-23 21:16:08 +0000

    App.js

--- 0x03-React_component/task_1/dashboard/src/App/App.js
+++ 0x03-React_component/task_1/dashboard/src/App/App.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.logOut = this.props.logOut || (() => {});
   }
+
   handleKeyDown = (event) => {
     if (event.ctrlKey && event.key === 'h') {
       alert('Logging you out');
       this.logOut();
     }
   };
-
   componentDidMount() {
     window.addEventListener('keydown', this.handleKeyDown);
   }
@@ -29,5 +30,13 @@ class App extends Component {
     );
   }
 }
+
+App.propTypes = {
+  logOut: PropTypes.func,
+};
+
+App.defaultProps = {
+  logOut: () => {},
+};
 
 export default App;
