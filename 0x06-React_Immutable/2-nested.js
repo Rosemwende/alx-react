8b3b6bb1814cba5288259585758f3a62826c45e6
rosemwende <rosemuema82@gmail.com>
2024-12-23 18:53:56 +0000

    2-nested.js

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -9,9 +9,6 @@ import { Map } from 'immutable';
 export default function accessImmutableObject(object, array) {
   const immutableObject = Map(object);
   const value = immutableObject.getIn(array);
-  if (Map.isMap(value)) {
-    return value;
-  }
-
+  
   return value;
 }
