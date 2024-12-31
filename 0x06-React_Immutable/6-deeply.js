c416df31db4f7a0a2433a64fe233c8963b5f6eb5
rosemwende <rosemuema82@gmail.com>
2024-12-31 21:21:17 +0000

    6-deeply.js

--- 0x06-React_Immutable/6-deeply.js
+++ 0x06-React_Immutable/6-deeply.js
@@ -3,8 +3,7 @@ import { Map } from 'immutable';
 const mergeDeeplyElements = (page1, page2) => {
   const map1 = Map(page1);
   const map2 = Map(page2);
-  const merged = map1.mergeDeep(map2);
-  return merged.toList();
+  return map1.mergeDeep(map2);
 };
 
 export default mergeDeeplyElements;
