ffcdce2503ca7a70691b1b2a52e46a09c672d840
rosemwende <rosemuema82@gmail.com>
2024-12-31 21:37:18 +0000

    8-seq.js

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -1,11 +1,16 @@
 import { Seq } from 'immutable';
 
 export default function printBestStudents(grades) {
-  Seq(grades)
+  const result = Seq(grades)
     .filter((student) => student.score >= 70)
-    .forEach((student) => {
-      const firstName = student.firstName.charAt(0).toUpperCase() + student.firstName.slice(1);
-      const lastName = student.lastName.charAt(0).toUpperCase() + student.lastName.slice(1);
-      console.log(`${firstName} ${lastName}`);
-    });
+    .map((student) => ({
+      firstName: student.firstName.charAt(0).toUpperCase() + student.firstName.slice(1),
+      lastName: student.lastName.charAt(0).toUpperCase() + student.lastName.slice(1)
+    }));
+
+  result.forEach((student) => {
+    console.log(`${student.firstName} ${student.lastName}`);
+  });
+
+  return result.toJS();
 }
