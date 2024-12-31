b056b1027a9967d101f054ae7c27c3d7d993ff69
rosemwende <rosemuema82@gmail.com>
2024-12-31 20:46:37 +0000

    2-nested.js

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,18 +1,13 @@
-import { Map } from 'immutable';
+import { fromJS } from 'immutable';
 
 /**
- * Access the value at a defined path in an immutable object using `getIn`
- * @param {Object} object - The object to access
- * @param {Array} array - The array defining the path to the value
- * @returns {any} - The value at the path (string, Map, or undefined)
+ * Converts a plain JavaScript object into an Immutable Map using fromJS.
+ * @param {Object} object - The plain object to convert.
+ * @returns {Map} - The Immutable Map object.
  */
-export default function accessImmutableObject(object, array) {
-  const immutableObject = Map(object);
-  const value = immutableObject.getIn(array);
-
-  if (value !== undefined) {
-    return value;
-  }
-
-  return undefined;
+export default function getImmutableObject(object) {
+  const immutableObject = fromJS(object);
+  
+  // Return the Immutable Map directly
+  return immutableObject;
 }
