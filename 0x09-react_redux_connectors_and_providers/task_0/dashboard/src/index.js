166d5450296dca5834e433034ea0c29e718d5a77
Rosemwende <rosemuema82@gmail.com>
2025-01-27 10:28:30 +0000

    index.js

--- 0x09-react_redux_connectors_and_providers/task_0/dashboard/src/index.js
+++ 0x09-react_redux_connectors_and_providers/task_0/dashboard/src/index.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import { connect } from 'react-redux';
-class App extends React.Component {
-  render() {
-    const { isLoggedIn } = this.props;
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import App from './App/App';
+import uiReducer from './reducers/uiReducer';
 
-    return (
-      <div>
-        <h1>Dashboard</h1>
-        {isLoggedIn ? (
-          <p>Welcome! You are logged in.</p>
-        ) : (
-          <p>Please log in to access your dashboard.</p>
-        )}
-      </div>
-    );
-  }
-}
+const store = createStore(uiReducer);
 
-const mapStateToProps = (state) => ({
-  isLoggedIn: state.uiReducer.isLoggedIn,
-});
-
-export default connect(mapStateToProps)(App);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
