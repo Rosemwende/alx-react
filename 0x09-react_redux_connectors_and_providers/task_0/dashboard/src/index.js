d2a4a3b4deea71b241612fa4b7c3ce60d51da27e
Rosemwende <rosemuema82@gmail.com>
2025-01-27 10:22:40 +0000

    index.js

---
+++ 0x09-react_redux_connectors_and_providers/task_0/dashboard/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { connect } from 'react-redux';
+class App extends React.Component {
+  render() {
+    const { isLoggedIn } = this.props;
+
+    return (
+      <div>
+        <h1>Dashboard</h1>
+        {isLoggedIn ? (
+          <p>Welcome! You are logged in.</p>
+        ) : (
+          <p>Please log in to access your dashboard.</p>
+        )}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  isLoggedIn: state.uiReducer.isLoggedIn,
+});
+
+export default connect(mapStateToProps)(App);
