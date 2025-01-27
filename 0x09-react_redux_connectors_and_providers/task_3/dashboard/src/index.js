774ef5bbeb3b9351d1c10165cac1f066aa44c3d1
Rosemwende <rosemuema82@gmail.com>
2025-01-27 14:21:31 +0000

    index.js

--- 0x09-react_redux_connectors_and_providers/task_3/dashboard/src/index.js
+++ 0x09-react_redux_connectors_and_providers/task_3/dashboard/src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { compose } from 'redux';
 import App from './App/App';
 import uiReducer from './reducers/uiReducer';
 
+const composeEnhancers = 
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(
   uiReducer,
-  composeWithDevTools(applyMiddleware(thunk))
+  composeEnhancers(applyMiddleware(thunk))
 );
 
 ReactDOM.render(
